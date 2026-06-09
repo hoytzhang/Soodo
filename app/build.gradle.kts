ed1ca4459b85a36bb2d1f741bb26c53a493ba862
name <name@mail.com>
2026-06-09 14:56:06 +0800

    去掉了打包混淆

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,15 +33,6 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("release")
-            isMinifyEnabled = true
-            isShrinkResources = true
-
-            proguardFiles(
-                getDefaultProguardFile(
-                    "proguard-android-optimize.txt"
-                ),
-                "proguard-rules.pro"
-            )
         }
     }
     compileOptions {
